8495bd571ef1b7a70245f8fc7f4b1ed8eb3da897
Stan-Nip <Jurka080604@gmail.com>
2026-04-07 14:50:42 +0300

    Remove .idea from repository, removed all the trash

--- Source/TestTask/TestTaskGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TestTaskGameMode.h"
-
-ATestTaskGameMode::ATestTaskGameMode()
-{
-	// stub
-}
--- Source/TestTask/Variant_Shooter/Weapons/ShooterWeaponHolder.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
-#include "ShooterWeaponHolder.h"
-
-// Add default functionality here for any IShooterWeaponHolder functions that are not pure virtual.
